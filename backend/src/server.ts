8dc29c376598c49c4e62fa978b69f8ec7524ff9c
Ryan Gomes <ryaanreeal@outlook.com>
2026-05-17 09:46:55 -0300

    feat(database): Configuração Prisma ORM com MongoDB e modelo inicial de usuário
    1. Instalação e configuração do Prisma ORM
    2. Conexão e configuração do banco de dados MongoDB
    3. Criação do schema do modelo inicial de Usuário
    4. Implementação de uma rota de API de teste para verificar a conexão

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,22 @@
 import Fastify from 'Fastify';
+import { routes } from './routes.js';
 import cors from '@fastify/cors';
 
 const app = Fastify({ logger: true })
+
+// Inicializa a API
+const startFastify = async()=>{
+
+    await app.register(cors);
+    await app.register(routes); // Carrega as rotas da API
+
+    try{
+        await app.listen({
+            port: 7938
+        })
+    }catch(err){
+        process.exit(1)
+    }
+}
+
+startFastify();
